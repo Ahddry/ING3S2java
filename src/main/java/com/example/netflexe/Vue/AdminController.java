6da62cc234dc9478c1d94049b39810e6fb674d53
pierrelouispaillet <57723732+pierrelouispaillet@users.noreply.github.com>
2022-03-19 15:20:49 +0100

    ajout photo admin controller

--- src/main/java/com/example/netflexe/Vue/AdminController.java
+++ src/main/java/com/example/netflexe/Vue/AdminController.java
@@ -3,6 +3,9 @@ package com.example.netflexe.Vue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.effect.SepiaTone;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class AdminController
 {
@@ -26,6 +29,28 @@ public class AdminController
         Button2.setText("Mon cinéma");
         Button3.setText("Statistiques");
         Button4.setText("Profil");
+
+
+        ImageView image = new ImageView(new Image("https://i.imgur.com/plCNG92.png"));
+        ImageView image2 = new ImageView(new Image("https://i.imgur.com/XsOIrxx.png"));
+        ImageView image3 = new ImageView(new Image("https://i.imgur.com/XN3tAzf.png"));
+        ImageView image4 = new ImageView(new Image("https://i.imgur.com/i78tBup.png"));
+        image.setFitWidth(40);
+        image.setFitHeight(50);
+        image2.setFitWidth(40);
+        image2.setFitHeight(50);
+        image3.setFitWidth(40);
+        image3.setFitHeight(50);
+        image4.setFitWidth(40);
+        image4.setFitHeight(50);
+        Button1.setText(null);
+        Button2.setText(null);
+        Button3.setText(null);
+        Button4.setText(null);
+        Button1.setGraphic(image);
+        Button2.setGraphic(image2);
+        Button3.setGraphic(image3);
+        Button4.setGraphic(image4);
         ToggleButton1.setText("Admin");
     }
 
