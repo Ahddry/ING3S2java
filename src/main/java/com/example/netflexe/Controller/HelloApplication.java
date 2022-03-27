f08ac8eb6170c36e94983662cf7c30009600f556
Arthur Fournier <arthur.g.fournier@gmail.com>
2022-03-27 17:00:29 +0200

    fix import

--- src/main/java/com/example/netflexe/Controller/HelloApplication.java
+++ src/main/java/com/example/netflexe/Controller/HelloApplication.java
@@ -8,9 +8,6 @@ import java.io.FileInputStream;
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import com.example.netflexe.Model.*;
 import com.example.netflexe.Model.Profil;
 import org.apache.commons.codec.digest.DigestUtils;
